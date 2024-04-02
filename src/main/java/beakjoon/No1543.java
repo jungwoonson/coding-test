e0b3fa9632ca89f4195ddfe0cef8eb7fe8f075f8
jungwoonson <jungwoon727@gmail.com>
2024-04-02 22:34:38 +0900

    백준 1543 풀이 개선

--- src/main/java/beakjoon/No1543.java
+++ src/main/java/beakjoon/No1543.java
@@ -4,24 +4,15 @@ public class No1543 {
 
     public int answer(String document, String value) {
         int answer = 0;
+        int startIndex = 0;
 
-        for (int i = 0; i < document.length(); i++) {
-            boolean isDuplicated = true;
-            for (int j = 0; j < value.length(); j++) {
-                if (i + j >= document.length()) {
-                    isDuplicated = false;
-                    break;
-                }
-
-                if (document.charAt(i + j) != value.charAt(j)) {
-                    isDuplicated = false;
-                    break;
-                }
+        while (true) {
+            int findIndex = document.indexOf(value, startIndex);
+            if (findIndex < 0) {
+                break;
             }
-            if (isDuplicated) {
-                answer++;
-                i += value.length() - 1;
-            }
+            startIndex = findIndex + value.length();
+            answer++;
         }
 
         return answer;
