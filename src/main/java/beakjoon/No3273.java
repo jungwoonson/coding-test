335ced49d72c5996fc2971537e5de8cad7b76ff7
jungwoonson <jungwoon727@gmail.com>
2024-04-05 23:57:16 +0900

    feat: 백준 3273 풀이

---
+++ src/main/java/beakjoon/No3273.java
@@ -0,0 +1,37 @@
+package beakjoon;
+
+import java.io.*;
+
+public class No3273 {
+
+    public static void answer() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.readLine();
+        String[] numberInputs = br.readLine().split(" ");
+        int[] numbers = new int[numberInputs.length];
+        for (int i = 0; i < numberInputs.length; i++) {
+            numbers[i] = Integer.parseInt(numberInputs[i]);
+        }
+        int x = Integer.parseInt(br.readLine());
+
+        int[] counts = new int[2000001];
+        for (int i = 0; i < numbers.length; i++) {
+            counts[numbers[i]]++;
+        }
+
+        int answer = 0;
+        int half = (x % 2 == 0) ? (x / 2 - 1) : (x /2);
+        for (int i = 1; i < half + 1; i++) {
+            if (counts[i] > 0 && counts[x - i] > 0) {
+                answer++;
+            }
+        }
+
+        bw.write(answer + "");
+
+        bw.flush();
+        bw.close();
+    }
+}
