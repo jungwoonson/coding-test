141e615c4fad716e44d65d5acbd5aeb3b0f5acca
jungwoonson <jungwoon727@gmail.com>
2024-04-05 23:56:56 +0900

    test: 백준 3273 테스트 케이스 추가

--- src/test/java/util/InOutputUtil.java
+++ src/test/java/util/InOutputUtil.java
@@ -1,14 +1,11 @@
 package util;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.PrintStream;
 
 public class InOutputUtil {
 
-    public static final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-    public static final PrintStream standardOut = System.out;
+    public static final String NL = System.lineSeparator();
 
     public static void setIn(String input) {
         InputStream in = generateUserInput(input);
