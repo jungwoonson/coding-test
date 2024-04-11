31c5b0be907aa3c454cc003fc29906417f3dcef2
jungwoonson <jungwoon727@gmail.com>
2024-04-11 15:19:04 +0900

    test: 백준 11068 테스트 케이스 추가

---
+++ src/test/java/beakjoon/No11068Test.java
@@ -0,0 +1,47 @@
+package beakjoon;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+import static beakjoon.InOutputUtil.NL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class No11068Test {
+
+    public final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(String.format("3%s747%s255%s946734", NL, NL, NL), String.format("1%s1%s0", NL, NL))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void main(String data, String expected) throws IOException {
+        InOutputUtil.setIn(data);
+
+        No11068.main(new String[0]);
+        String actual = outputStreamCaptor.toString().trim();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
