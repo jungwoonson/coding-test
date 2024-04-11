4fe32d2e41488d17d80c4307f972c6a75dd1cdc8
jungwoonson <jungwoon727@gmail.com>
2024-04-11 14:43:22 +0900

    feat: 백준 11005 풀이

--- src/main/java/beakjoon/No11005.java
+++ src/main/java/beakjoon/No11005.java
@@ -8,10 +8,30 @@ public class No11005 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("");
+        String[] data = br.readLine().split(" ");
+        int number = Integer.parseInt(data[0]);
+        final int b = Integer.parseInt(data[1]);
+        StringBuilder answer = new StringBuilder();
+
+        do {
+            answer.insert(0, conversion(number % b));
+            number = number / b;
+        } while (number / b != 0);
+
+        if (number != 0) {
+            answer.insert(0, conversion(number));
+        }
+
+        bw.write(answer.toString());
 
         bw.flush();
         bw.close();
     }
-}
 
+    public static String conversion(int number) {
+        if (number < 10) {
+            return String.valueOf((char) ('0' + number));
+        }
+        return String.valueOf((char) (number - 10 + 'A'));
+    }
+}
