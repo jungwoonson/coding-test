fa20fda85c77d75a2481f271ef7a2526fb665fee
jungwoonson <jungwoon727@gmail.com>
2024-04-11 15:56:35 +0900

    feat: 백준 11068 풀이

--- src/main/java/beakjoon/No11068.java
+++ src/main/java/beakjoon/No11068.java
@@ -8,10 +8,44 @@ public class No11068 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        bw.write("");
+        int count = Integer.parseInt(br.readLine());
+        for (int i = 0; i < count; i++) {
+            int number = Integer.parseInt(br.readLine());
+            boolean flag = true;
+            for (int j = 2; j <= 64 ; j++) {
+                String conversionNum = conversion(number, j);
+                if (isPalindrome(conversionNum)) {
+                    bw.write("1" + System.lineSeparator());
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                bw.write("0" + System.lineSeparator());
+            }
+        }
 
         bw.flush();
         bw.close();
+    }
+
+    public static String conversion(final int number, final int b) {
+        int value = number;
+        StringBuilder result = new StringBuilder();
+        while (value > 0) {
+            result.append((char) ((value % b) + '0'));
+            value = value / b;
+        }
+        return result.toString();
+    }
+
+    public static boolean isPalindrome(String value) {
+        for (int i = 0; i < value.length(); i++) {
+            if (value.charAt(i) != value.charAt(value.length() - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 
