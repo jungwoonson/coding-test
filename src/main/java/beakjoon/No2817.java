147dbe6c780a52f70088ecb8d5e3092c6b2e4e6e
jungwoonson <jungwoon727@gmail.com>
2024-04-23 18:44:14 +0900

    feat: 백준 2817 문제푸는 중

--- src/main/java/beakjoon/No2817.java
+++ src/main/java/beakjoon/No2817.java
@@ -8,6 +8,24 @@ public class No2817 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int participant = Integer.parseInt(br.readLine());
+        int count = Integer.parseInt(br.readLine());
+        int[] staffs = new int[26];
+        double[][] result = new double[26][14];
+
+        for (int i = 0; i < count; i++) {
+            String[] info = br.readLine().split(" ");
+            char name = info[0].charAt(0);
+            int vote = Integer.parseInt(info[1]);
+            if (vote < participant * 0.05) {
+                continue;
+            }
+
+            for (int j = 0; j < 14; j++) {
+                result[name - 'A'][j] = (double) vote / (double) (j + 1);
+            }
+        }
+
         bw.write("");
 
         bw.flush();
