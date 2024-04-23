ad8aec181805eef85912b4480aa8b17b753a571e
jungwoonson <jungwoon727@gmail.com>
2024-04-23 17:46:16 +0900

    test: 백준 2817 테스트 케이스 추가

---
+++ src/main/java/beakjoon/No2817.java
@@ -0,0 +1,17 @@
+package beakjoon;
+
+import java.io.*;
+
+public class No2817 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("");
+
+        bw.flush();
+        bw.close();
+    }
+}
+
