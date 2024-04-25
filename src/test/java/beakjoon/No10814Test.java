e28fe747f8a1b589a85f8089f24530b8eb9da2d6
jungwoonson <jungwoon727@gmail.com>
2024-04-25 17:08:29 +0900

    feat: 백준 10814 풀이

---
+++ src/test/java/beakjoon/No10814Test.java
@@ -0,0 +1,48 @@
+package beakjoon;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+import static beakjoon.InOutputUtil.NL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class No10814Test {
+
+    public final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of("3" + NL + "21 Junkyu" + NL + "21 Dohyun" + NL + "20 Sunyoung",
+                        "20 Sunyoung" + NL + "21 Junkyu" + NL + "21 Dohyun")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void main(String data, String expected) throws IOException {
+        InOutputUtil.setIn(data);
+
+        No10814.main(new String[0]);
+        String actual = outputStreamCaptor.toString().trim();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
