992d7c7beba0a6cbcbcd87b4fdb3f86d8500c352
jungwoonson <jungwoon727@gmail.com>
2024-05-13 20:41:33 +0900

    feat: 백준 7785 풀이

--- src/main/java/beakjoon/no005정렬/No7785.java
+++ src/main/java/beakjoon/no005정렬/No7785.java
@@ -10,48 +10,23 @@ public class No7785 {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int count = Integer.parseInt(br.readLine());
-        String[] infos = new String[count];
+        String[][] infos = new String[count][2];
 
         for (int i = 0; i < infos.length; i++) {
-            infos[i] = br.readLine();
+            String[] record = br.readLine().split(" ");
+            infos[i][0] = record[0];
+            infos[i][1] = record[1];
         }
 
-        Arrays.sort(infos, (o1, o2) -> {
-            String[] o1Split = o1.split(" ");
-            String[] o2Split = o2.split(" ");
+        Arrays.sort(infos, (o1, o2) -> o2[0].compareTo(o1[0]));
 
-            String o1Name = o1Split[0];
-            String o2Name = o2Split[0];
-
-            String o1Value = o1Split[1];
-            String o2Value = o2Split[1];
-
-            if (o1Name.compareTo(o2Name) == 0) {
-                return o1Value.compareTo(o2Value);
+        for (int i = 0; i < count - 1; i++) {
+            if (infos[i][1].equals("enter") && !infos[i][0].equals(infos[i + 1][0])) {
+                bw.write(infos[i][0] + System.lineSeparator());
             }
-
-            return o2Name.compareTo(o1Name);
-        });
-
-        String name = infos[0].split(" ")[0];
-        int nameCount = 1;
-        for (int i = 1; i < infos.length; i++) {
-            String nowName = infos[i].split(" ")[0];
-            if (name.equals(nowName)) {
-                nameCount++;
-                continue;
-            }
-
-            if (nameCount % 2 == 1) {
-                bw.write(name + System.lineSeparator());
-            }
-
-            nameCount = 1;
-            name = nowName;
-
-            if (i == infos.length - 1) {
-                bw.write(nowName + System.lineSeparator());
-            }
+        }
+        if (infos[count - 1][1].equals("enter")) {
+            bw.write(infos[count - 1][0] + System.lineSeparator());
         }
 
         bw.flush();
