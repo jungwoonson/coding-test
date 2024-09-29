692a640cc189874da7870b5a9fd63e7e0363c95e
jungwoonson <jungwoon727@gmail.com>
2024-09-29 23:13:41 +0900

    fix: 백준 1654 풀이

---
+++ src/test/java/beakjoon/no007이분탐색/No1654Test.java
@@ -0,0 +1,53 @@
+package beakjoon.no007이분탐색;
+
+import beakjoon.InOutputUtil;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.stream.Stream;
+
+import static beakjoon.InOutputUtil.NL;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class No1654Test {
+
+    public final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(System.out);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of("4 11" + NL +
+                        "802" + NL +
+                        "743" + NL +
+                        "457" + NL +
+                        "539",
+                        "200")
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void main(String data, String expected) throws IOException {
+        InOutputUtil.setIn(data);
+
+        No1654.main(new String[0]);
+        String actual = outputStreamCaptor.toString().trim();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
