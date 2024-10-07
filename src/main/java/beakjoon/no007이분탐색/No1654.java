7c293f47347505026c088554f9423279680aa892
jungwoonson <jungwoon727@gmail.com>
2024-10-07 23:30:07 +0900

    fix: 백준 1654 풀이

--- src/main/java/beakjoon/no007이분탐색/No1654.java
+++ src/main/java/beakjoon/no007이분탐색/No1654.java
@@ -8,6 +8,42 @@ public class No1654 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        String[] inputs = br.readLine().split(" ");
+        int k = Integer.parseInt(inputs[0]);
+        int n = Integer.parseInt(inputs[1]);
+
+        int[] lines = new int[k];
+
+        long max = 0;
+        for (int i = 0; i < k; i++) {
+            lines[i] = Integer.parseInt(br.readLine());
+            if(max < lines[i]) {
+                max = lines[i];
+            }
+        }
+
+        long start = 1;
+        long end = max;
+        long answer = 0;
+
+        while (start <= end) {
+            long middle = (start + end) / 2;
+            long lanCount = 0;
+
+            for (int i = 0; i < k; i++) {
+                lanCount += lines[i] / middle;
+            }
+
+            if (lanCount >= n) {
+                answer = middle;
+                start = middle + 1;
+            } else {
+                end = middle - 1;
+            }
+        }
+
+        bw.write(answer + System.lineSeparator());
+
         bw.flush();
         bw.close();
     }
