f050d86eab6f9e9af24c1379d85db5b25a1c8370
jungwoonson <jungwoon727@gmail.com>
2025-05-10 19:25:57 +0900

    feat: ReetCode > Array101 > Inserting Items Into an Array 풀이
    - https://leetcode.com/explore/learn/card/fun-with-arrays/525/inserting-items-into-an-array/3243/

---
+++ src/main/java/reetcode/array/MergeSortedArray.java
@@ -0,0 +1,21 @@
+package reetcode.array;
+
+public class MergeSortedArray {
+    // Follow up: Can you come up with an algorithm that runs in O(m + n) time?
+    // 이미 정렬되어 있기 때문에 투 포인터를 사용해서 뒤에서부터 num1과 num2를 비교하며 채운다
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index = m + n - 1;
+        m--;
+        n--;
+        while (index >= 0) {
+            if (n < 0) {
+                return;
+            }
+            if (m < 0 || nums1[m] <= nums2[n]) {
+                nums1[index--] = nums2[n--];
+                continue;
+            }
+            nums1[index--] = nums1[m--];
+        }
+    }
+}
