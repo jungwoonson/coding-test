cd668fe0f5e55fa9c252fb481b4682730492cbfa
jungwoonson <jungwoon727@gmail.com>
2025-05-13 10:09:36 +0900

    feat: LeetCode > Array101 > Searching for Items in an Array 풀이
    https://leetcode.com/explore/learn/card/fun-with-arrays/527/searching-for-items-in-an-array/

---
+++ src/test/java/reetcode/array/ValidMountainArrayTest.java
@@ -0,0 +1,27 @@
+package reetcode.array;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class ValidMountainArrayTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void test(int[] arr, boolean expected) {
+        boolean actual = new ValidMountainArray().validMountainArray(arr);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(new int[] {2,1}, false),
+                Arguments.of(new int[] {3,5,5}, false),
+                Arguments.of(new int[] {0,3,2,1}, true),
+                Arguments.of(new int[] {2}, false)
+        );
+    }
+}
