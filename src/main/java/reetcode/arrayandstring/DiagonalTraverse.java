30bf14099b1ae49e08dcd7465d00d3e18f7b39c4
jungwoonson <jungwoon727@gmail.com>
2025-05-20 17:22:33 +0900

    feat: LeetCode > Array and String > Diagonal Traverse 풀이
    https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1167/

---
+++ src/main/java/reetcode/arrayandstring/DiagonalTraverse.java
@@ -0,0 +1,37 @@
+package reetcode.arrayandstring;
+
+public class DiagonalTraverse {
+    // Constraints:
+    // m == mat.length
+    // n == mat[i].length
+    // 1 <= m, n <= 10^4
+    // 1 <= m * n <= 10^4
+    // -10^5 <= mat[i][j] <= 10^5
+
+    // (0,0) | (0,1) (1,0) | (2,0) (1,1) (0,2) | (1,2) (2,1) | (2,2)
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] result = new int[m * n];
+        int idx = 0;
+
+        for (int d = 0; d < m + n - 1; d++) {
+            int r, c;
+            if (d % 2 == 0) {
+                r = d < m ? d : m - 1;
+                c = d - r;
+                while (r >= 0 && c < n) {
+                    result[idx++] = mat[r--][c++];
+                }
+            } else {
+                c = d < n ? d : n - 1;
+                r = d - c;
+                while (c >= 0 && r < m) {
+                    result[idx++] = mat[r++][c--];
+                }
+            }
+        }
+
+        return result;
+    }
+}
