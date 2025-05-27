f00d803313d72ac6947191b27188767ff07fa7b5
jungwoonson <jungwoon727@gmail.com>
2025-05-27 14:04:47 +0900

    feat: LeetCode > Array and String > Two Sum, Minimum Size Subarray Sum 풀이
    https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1153/
    https://leetcode.com/explore/learn/card/array-and-string/205/array-two-pointer-technique/1299/

---
+++ src/test/java/reetcode/arrayandstring/TwoSumTest.java
@@ -0,0 +1,26 @@
+package reetcode.arrayandstring;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class TwoSumTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void test(int[] numbers, int target, int[] expected) {
+        int[] actual = new TwoSum().twoSum(numbers, target);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(new int[]{2, 7, 11, 15}, 9, new int[]{1, 2}),
+                Arguments.of(new int[]{2, 3, 4}, 6, new int[]{1, 3}),
+                Arguments.of(new int[]{-1, 0}, -1, new int[]{1, 2})
+        );
+    }
+}
