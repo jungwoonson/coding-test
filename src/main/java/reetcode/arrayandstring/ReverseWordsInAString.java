2255e497681f9338980d248beb0dd605a09168c8
jungwoonson <jungwoon727@gmail.com>
2025-05-29 14:27:10 +0900

    feat: LeetCode > Array and String > Reverse Words In A String 풀이
    https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1164/
    https://leetcode.com/explore/learn/card/array-and-string/204/conclusion/1165/

---
+++ src/main/java/reetcode/arrayandstring/ReverseWordsInAString.java
@@ -0,0 +1,29 @@
+package reetcode.arrayandstring;
+
+public class ReverseWordsInAString {
+    // Constraints:
+    // 1 <= s.length <= 10^4
+    // s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+    // There is at least one word in s.
+    public String reverseWords(String s) {
+        s = " " + s;
+        StringBuilder result = new StringBuilder();
+        boolean flag = false;
+        int rightTarget = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (!flag && s.charAt(i) != ' ') {
+                rightTarget = i;
+                flag = true;
+                continue;
+            }
+            if (flag && s.charAt(i) == ' ') {
+                flag = false;
+                for (int j = i + 1; j <= rightTarget; j++) {
+                    result.append(s.charAt(j));
+                }
+                result.append(' ');
+            }
+        }
+        return result.toString().trim();
+    }
+}
