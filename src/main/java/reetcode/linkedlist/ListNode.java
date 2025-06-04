a9733611c87129f000d6acc3531faf3b1665a380
jungwoonson <jungwoon727@gmail.com>
2025-06-04 16:35:18 +0900

    feat: LeetCode > Array and String > Remove Nth Node From End of List
    https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1296/

--- src/main/java/reetcode/linkedlist/ListNode.java
+++ src/main/java/reetcode/linkedlist/ListNode.java
@@ -4,8 +4,16 @@ class ListNode {
     int val;
     ListNode next;
 
+    ListNode() {
+    }
+
     ListNode(int x) {
         val = x;
         next = null;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
 }
