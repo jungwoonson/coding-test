31f7ae06b646076a8be4c9d962b3e2b58f34fbc0
jungwoonson <jungwoon727@gmail.com>
2025-06-04 09:15:01 +0900

    문제있는 테스트케이스 수정

--- src/test/java/reetcode/linkedlist/LinkedListCycle2Test.java
+++ src/test/java/reetcode/linkedlist/LinkedListCycle2Test.java
@@ -2,55 +2,47 @@ package reetcode.linkedlist;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.stream.Stream;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 class LinkedListCycle2Test {
 
-    @ParameterizedTest
-    @MethodSource("data")
-    void test(ListNode head, ListNode expected) {
-        ListNode actual = new LinkedListCycle2().detectCycle(head);
-        assertThat(actual).isEqualTo(expected);
-    }
-
-    public static Stream<Arguments> data() {
-        return Stream.of(
-                Arguments.of(example1(), new ListNode(2)),
-                Arguments.of(example2(), new ListNode(1)),
-                Arguments.of(example3(), null)
-        );
-    }
-
-    private static ListNode example1() {
+    @Test
+    void test1() {
         ListNode node0 = new ListNode(3);
         ListNode node1 = new ListNode(2);
         ListNode node2 = new ListNode(0);
         ListNode node3 = new ListNode(-4);
-
         node0.next = node1;
         node1.next = node2;
         node2.next = node3;
         node3.next = node1;
+        LinkedListCycle2 linkedListCycle2 = new LinkedListCycle2();
 
-        return node0;
+        ListNode actual = linkedListCycle2.detectCycle(node0);
+
+        assertThat(actual).isEqualTo(node1);
     }
 
-    private static ListNode example2() {
+    @Test
+    void test2() {
         ListNode node0 = new ListNode(1);
         ListNode node1 = new ListNode(2);
-
         node0.next = node1;
         node1.next = node0;
+        LinkedListCycle2 linkedListCycle2 = new LinkedListCycle2();
 
-        return node0;
+        ListNode actual = linkedListCycle2.detectCycle(node0);
+
+        assertThat(actual).isEqualTo(node0);
     }
 
-    private static ListNode example3() {
+    @Test
+    void test3() {
         ListNode node0 = new ListNode(3);
+        LinkedListCycle2 linkedListCycle2 = new LinkedListCycle2();
 
-        return node0;
+        ListNode actual = linkedListCycle2.detectCycle(node0);
+
+        assertThat(actual).isEqualTo(null);
     }
 }
