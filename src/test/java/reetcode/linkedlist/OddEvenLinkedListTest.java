77d58501810102441f5c3eef284ad9455e9897b4
jungwoonson <jungwoon727@gmail.com>
2025-06-09 16:44:28 +0900

    feat: LeetCode > Linked List > Remove Linked List Elements, Odd Even Linked List
    https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
    https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1208/

---
+++ src/test/java/reetcode/linkedlist/OddEvenLinkedListTest.java
@@ -0,0 +1,44 @@
+package reetcode.linkedlist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class OddEvenLinkedListTest {
+
+    @Test
+    void test1() {
+        ListNode node4 = new ListNode(5);
+        ListNode node3 = new ListNode(4, node4);
+        ListNode node2 = new ListNode(3, node3);
+        ListNode node1 = new ListNode(2, node2);
+        ListNode node0 = new ListNode(1, node1);
+
+        ListNode actual = new OddEvenLinkedList().oddEvenList(node0);
+
+        check(actual, new int[]{1, 3, 5, 2, 4});
+    }
+
+    @Test
+    void test2() {
+        ListNode node6 = new ListNode(7);
+        ListNode node5 = new ListNode(4, node6);
+        ListNode node4 = new ListNode(6, node5);
+        ListNode node3 = new ListNode(5, node4);
+        ListNode node2 = new ListNode(3, node3);
+        ListNode node1 = new ListNode(1, node2);
+        ListNode node0 = new ListNode(2, node1);
+
+        ListNode actual = new OddEvenLinkedList().oddEvenList(node0);
+
+        check(actual, new int[]{2, 3, 6, 7, 1, 5, 4});
+    }
+
+    private static void check(ListNode actual, int[] expectedValues) {
+        ListNode current = actual;
+        for (int value : expectedValues) {
+            assertThat(current.val).isEqualTo(value);
+            current = current.next;
+        }
+    }
+}
