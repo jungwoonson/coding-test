3311c48b65688c9cb86de49e08886cdd10c14532
jungwoonson <jungwoon727@gmail.com>
2025-06-10 11:10:49 +0900

    feat: LeetCode > Linked List > Palindrome Linked List
    https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1209/

---
+++ src/test/java/reetcode/linkedlist/PalindromeLinkedListTest.java
@@ -0,0 +1,30 @@
+package reetcode.linkedlist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class PalindromeLinkedListTest {
+
+    @Test
+    void test1() {
+        ListNode node3 = new ListNode(1);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(2, node2);
+        ListNode node0 = new ListNode(1, node1);
+
+        boolean actual = new PalindromeLinkedList().isPalindrome(node0);
+
+        assertThat(actual).isTrue();
+    }
+
+    @Test
+    void test2() {
+        ListNode node1 = new ListNode(2);
+        ListNode node0 = new ListNode(1, node1);
+
+        boolean actual = new PalindromeLinkedList().isPalindrome(node0);
+
+        assertThat(actual).isFalse();
+    }
+}
