cc3b2d5e3ac3db10a3d786659f1d8452dfdc1962
jungwoonson <jungwoon727@gmail.com>
2025-06-20 11:47:28 +0900

    feat: LeetCode > Queue & Stack > Perfect Squares
    https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1371/

---
+++ src/main/java/reetcode/queue/PerfectSquares.java
@@ -0,0 +1,42 @@
+package reetcode.queue;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class PerfectSquares {
+
+    // Constraints:
+    // 1 <= n <= 10^4
+    public int numSquares(int n) {
+        List<Integer> squares = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++) {
+            squares.add(i * i);
+        }
+        squares.sort(Comparator.reverseOrder());
+
+        Queue<Integer> queue = new LinkedList<>();
+        int count = 1;
+        queue.add(n);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int origin = queue.poll();
+                for (Integer square : squares) {
+                    int result = origin - square;
+                    if (result == 0) {
+                        return count;
+                    }
+                    if (result > 0) {
+                        queue.add(result);
+                    }
+                }
+            }
+            count++;
+        }
+
+        return -1;
+    }
+}
