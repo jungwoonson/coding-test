b6e7da7b276e011436ef716254c331923ec6282b
jungwoonson <jungwoon727@gmail.com>
2025-06-20 11:47:28 +0900

    feat: LeetCode > Queue & Stack > Perfect Squares
    https://leetcode.com/explore/learn/card/queue-stack/231/practical-application-queue/1371/

---
+++ src/test/java/reetcode/queue/PerfectSquaresTest.java
@@ -0,0 +1,25 @@
+package reetcode.queue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class PerfectSquaresTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void test(int n, int expected) {
+        int actual = new PerfectSquares().numSquares(n);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(12, 3),
+                Arguments.of(13, 2)
+        );
+    }
+}
