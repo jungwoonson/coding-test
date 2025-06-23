cdd4bea8de4eddcd9eb81499d547064bf3a3d4cd
jungwoonson <jungwoon727@gmail.com>
2025-06-23 10:17:39 +0900

    feat: LeetCode > Queue & Stack > Valid Parentheses
    https://leetcode.com/explore/learn/card/queue-stack/230/usage-stack/1361/

---
+++ src/main/java/reetcode/queue/ValidParentheses.java
@@ -0,0 +1,29 @@
+package reetcode.queue;
+
+import java.util.Stack;
+
+class ValidParentheses {
+
+    // Constraints:
+    // 1 <= s.length <= 10^4
+    // s consists of parentheses only '()[]{}'.
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char bracket = s.charAt(i);
+            if (bracket == '(' || bracket == '{' || bracket == '[') {
+                stack.push(bracket);
+                continue;
+            }
+            if (stack.empty()) {
+                return false;
+            }
+            Character pop = stack.pop();
+            if ((bracket == ')' && pop == '(') || (bracket == '}' && pop == '{') || (bracket == ']' && pop == '[')) {
+                continue;
+            }
+            return false;
+        }
+        return stack.isEmpty();
+    }
+}
