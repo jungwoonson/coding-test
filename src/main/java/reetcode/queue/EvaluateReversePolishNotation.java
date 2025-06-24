eaa87c2dac6f3e1ac4f772d249fc2c112e1681e3
jungwoonson <jungwoon727@gmail.com>
2025-06-24 23:53:29 +0900

    feat: LeetCode > Queue & Stack > Evaluate Reverse Polish Notation
    - https://leetcode.com/explore/learn/card/queue-stack/230/usage-stack/1394/

---
+++ src/main/java/reetcode/queue/EvaluateReversePolishNotation.java
@@ -0,0 +1,38 @@
+package reetcode.queue;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+class EvaluateReversePolishNotation {
+
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperator(token, a, b));
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return "+-*/".contains(token);
+    }
+
+    private int applyOperator(String op, int a, int b) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: throw new IllegalArgumentException("Invalid operator: " + op);
+        }
+    }
+}
