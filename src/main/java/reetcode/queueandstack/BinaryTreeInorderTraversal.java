8e6f738e51c2251f0fbedb97452cde4be80de154
jungwoonson <jungwoon727@gmail.com>
2025-06-28 17:17:11 +0900

    feat: ReetCode > Binary Tree Inorder Traversal
    https://leetcode.com/explore/learn/card/queue-stack/232/practical-application-stack/1383/

---
+++ src/main/java/reetcode/queueandstack/BinaryTreeInorderTraversal.java
@@ -0,0 +1,47 @@
+package reetcode.queueandstack;
+
+import java.util.*;
+
+class BinaryTreeInorderTraversal {
+
+    // Constraints:
+    // The number of nodes in the tree is in the range [0, 100].
+    // -100 <= Node.val <= 100
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
