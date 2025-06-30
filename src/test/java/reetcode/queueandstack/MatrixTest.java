a2e688b9a0e9d6b40b2fb0da1957e24d1171a168
jungwoonson <jungwoon727@gmail.com>
2025-06-30 18:19:55 +0900

    feat: ReetCode > Queue & Stack > 01 Matrix
    https://leetcode.com/explore/learn/card/queue-stack/239/conclusion/1388/

---
+++ src/test/java/reetcode/queueandstack/MatrixTest.java
@@ -0,0 +1,41 @@
+package reetcode.queueandstack;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MatrixTest {
+
+    @ParameterizedTest
+    @MethodSource("data")
+    void test(int[][] image, int[][] expected) {
+        int[][] actual = new Matrix().updateMatrix(image);
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    public static Stream<Arguments> data() {
+        return Stream.of(
+                Arguments.of(
+                        new int[][]{
+                                {0, 0, 0},
+                                {0, 1, 0},
+                                {0, 0, 0}},
+                        new int[][]{
+                                {0, 0, 0},
+                                {0, 1, 0},
+                                {0, 0, 0}}),
+                Arguments.of(
+                        new int[][]{
+                                {0, 0, 0},
+                                {0, 1, 0},
+                                {1, 1, 1}},
+                        new int[][]{
+                                {0, 0, 0},
+                                {0, 1, 0},
+                                {1, 2, 1}})
+        );
+    }
+}
