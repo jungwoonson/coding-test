3e93bfa953169ed309c8deb74f40dd2e52c37790
jungwoonson <jungwoon727@gmail.com>
2025-07-04 11:49:12 +0900

    feat: ReetCode > Hash Table > Two Sum, Isomorphic Strings, Minimum Index Sum of Two Lists
    https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1115/
    https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1117/
    https://leetcode.com/explore/learn/card/hash-table/184/comparison-with-other-data-structures/1177/

---
+++ src/main/java/reetcode/hashtable/IsomorphicStrings.java
@@ -0,0 +1,35 @@
+package reetcode.hashtable;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+
+    // Constraints:
+    // 1 <= s.length <= 5 * 10^4
+    // t.length == s.length
+    // s and t consist of any valid ascii character.
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if (map.containsKey(c1) && map.get(c1) != c2) {
+                return false;
+            }
+            map.put(c1, c2);
+        }
+
+        map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c1 = t.charAt(i);
+            char c2 = s.charAt(i);
+            if (map.containsKey(c1) && map.get(c1) != c2) {
+                return false;
+            }
+            map.put(c1, c2);
+        }
+
+        return true;
+    }
+}
