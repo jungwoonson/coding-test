7f136551b3c9e73a45ab3c79b338733ee39591f0
jungwoonson <jungwoon727@gmail.com>
2025-07-15 14:33:05 +0900

    feat: ReetCode > Binary Search > Binary Search, Sqrt(x), Guess Number Higher or Lower, Search in Rotated Sorted Array
    https://leetcode.com/explore/learn/card/binary-search/138/background/1038/
    https://leetcode.com/explore/learn/card/binary-search/125/template-i/950/
    https://leetcode.com/explore/learn/card/binary-search/125/template-i/951/
    https://leetcode.com/explore/learn/card/binary-search/125/template-i/952/

---
+++ src/main/java/reetcode/binarysearch/SqrtX.java
@@ -0,0 +1,29 @@
+package reetcode.binarysearch;
+
+public class SqrtX {
+
+    // Constraints:
+    // 0 <= x <= 2^31 - 1
+    public int mySqrt(int x) {
+        long low = 0;
+        long high = 46341;
+
+        while (low <= high) {
+            long mid = low + (high - low) / 2;
+            long pow1 = mid * mid;
+            long pow2 = (mid + 1) * (mid + 1);
+            if (pow1 <= x && x < pow2) {
+                return (int) mid;
+            }
+            if (pow1 > x) {
+                high = mid - 1;
+                continue;
+            }
+            if (pow2 <= x) {
+                low = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
