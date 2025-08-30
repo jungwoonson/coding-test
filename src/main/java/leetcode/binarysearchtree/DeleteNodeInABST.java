f862139a00bab7ae47a2bece4e5d6057183d8b30
jungwoonson <jungwoon727@gmail.com>
2025-08-30 18:03:32 +0900

    feat: LeetCode > Binary Search Tree > Delete Node in a BST
    - https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/141/basic-operations-in-a-bst/1006/

---
+++ src/main/java/leetcode/binarysearchtree/DeleteNodeInABST.java
@@ -0,0 +1,45 @@
+package leetcode.binarysearchtree;
+
+public class DeleteNodeInABST {
+
+    // Constraints:
+    // The number of nodes in the tree is in the range [0, 10^4].
+    // -10^5 <= Node.val <= 10^5
+    // Each node has a unique value.
+    // root is a valid binary search tree.
+    // -10^5 <= key <= 10^5
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+
+        if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+            return root;
+        }
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+            return root;
+        }
+
+        if (root.left == null) {
+            return root.right;
+        }
+
+        if (root.right == null) {
+            return root.left;
+        }
+
+        TreeNode succ = minNode(root.right);
+        root.val = succ.val;
+        root.right = deleteNode(root.right, succ.val);
+
+        return root;
+    }
+
+    private TreeNode minNode(TreeNode node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
+}
