d264f7a301ab36245a9a870347af1b274576b02e
jungwoonson <jungwoon727@gmail.com>
2025-09-04 23:04:46 +0900

    feat: LeetCode > Binary Search Tree > Kth Largest Element in a Stream
    - https://leetcode.com/explore/learn/card/introduction-to-data-structure-binary-search-tree/142/conclusion/1018/

--- src/main/java/leetcode/binarysearchtree/KthLargest.java
+++ src/main/java/leetcode/binarysearchtree/KthLargest.java
@@ -1,7 +1,5 @@
 package leetcode.binarysearchtree;
 
-import java.util.PriorityQueue;
-
 public class KthLargest {
 
     // Constraints:
@@ -9,26 +7,78 @@ public class KthLargest {
     // 1 <= k <= nums.length + 1
     // -10^4 <= nums[i] <= 10^4
     // -10^4 <= val <= 10^4
-    // At most 104 calls will be made to add.
-
-    private final int k;
-    private final PriorityQueue<Integer> pq = new PriorityQueue<>();
+    // At most 10^4 calls will be made to add.
+    Node root;
+    int k;
 
     public KthLargest(int k, int[] nums) {
         this.k = k;
-        for (int n : nums) {
-            pq.offer(n);
-            if (pq.size() > k) {
-                pq.poll();
-            }
+        if (nums == null || nums.length == 0) {
+            return;
+        }
+
+        this.root = new Node(nums[0]);
+        for (int i = 1; i < nums.length; i++) {
+            insert(root, nums[i]);
         }
     }
 
     public int add(int val) {
-        pq.offer(val);
-        if (pq.size() > k) {
-            pq.poll();
+        if (this.root == null) {
+            this.root = new Node(val);
+            return search(root, k);
         }
-        return pq.peek();
+
+        insert(root, val);
+        return search(root, k);
+    }
+
+    public void insert(Node root, int val) {
+        if (root == null) {
+            return;
+        }
+
+        root.count++;
+        if (root.val >= val) {
+            if (root.left == null) {
+                root.left = new Node(val);
+                return;
+            }
+            insert(root.left, val);
+            return;
+        }
+
+        if (root.right == null) {
+            root.right = new Node(val);
+            return;
+        }
+
+        insert(root.right, val);
+    }
+
+    private int search(Node root, int k) {
+        int right = root.right == null ? 0 : root.right.count;
+
+        if (right + 1 == k) {
+            return root.val;
+        }
+
+        if (right + 1 > k) {
+            return search(root.right, k);
+        }
+
+        return search(root.left, k - right - 1);
+    }
+
+    class Node {
+        int val;
+        Node left;
+        Node right;
+        int count;
+
+        public Node(int val) {
+            this.val = val;
+            this.count = 1;
+        }
     }
 }
