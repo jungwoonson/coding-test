15e286032530175fba4feae253b454ed0aa4fef0
jungwoonson <jungwoon727@gmail.com>
2025-09-29 14:50:41 +0900

    refactor: LeetCode > recursion > Swap Nodes in Pairs
    - https://leetcode.com/explore/learn/card/recursion-i/250/principle-of-recursion/1681/

--- src/main/java/leetcode/recursion/SwapNodesInPairs.java
+++ src/main/java/leetcode/recursion/SwapNodesInPairs.java
@@ -3,22 +3,14 @@ package leetcode.recursion;
 public class SwapNodesInPairs {
 
     public ListNode swapPairs(ListNode head) {
-        return recursion(head, null);
-    }
-
-    public ListNode recursion(ListNode head, ListNode before) {
         if (head == null || head.next == null) {
             return head;
         }
 
-        ListNode next = head.next.next;
+        ListNode next = swapPairs(head.next.next);
         ListNode newHead = head.next;
-        if (before != null) {
-            before.next = newHead;
-        }
         newHead.next = head;
         head.next = next;
-        recursion(head.next, head);
         return newHead;
     }
 
